a188671866936a94d1d7b4b733d18740e09806e9
Groot <gabrielmiwveras@gmail.com>
2022-02-07 23:28:02 -0300

    ...

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ import os
 
 device = os.open("/dev/midi2", os.O_RDWR)
 
+def edit_mode():
+    try:
+        os.write(device, bytearray(zoom.edit_mode[sys.argv[2].lower()]))
+        os.system("clear")
+
+        if sys.argv[2].upper() == "ON":
+            print(f"EDIT MODE: \033[05m\033[92m{sys.argv[2].upper()}\033[0m")
+
+        if sys.argv[2].upper() == "OFF":
+            print(f"EDIT MODE: \033[05m\033[91m{sys.argv[2].upper()}\033[0m")
+
+        os.close(device)
+
+    except IndexError:
+        os.system("clear")
+        print("\033[93mWarning!\033[0m",
+        "Missing Arguments: ON\OFF")
+        os.close(device)
+
+    except KeyError:
+        os.system("clear")
+        print("\033[93mInvalid Argument\033[0m:",
+        f"\033[93m{sys.argv[2].upper()}\033[0m is not a valid argument!",
+        "Just \033[93mON\033[0m or \033[93mOFF\033[0m!")
+        os.close(device)
+
 def tuner():
     try:
         os.write(device, bytearray(zoom.tuner[sys.argv[2].lower()]))
@@ -23,14 +49,46 @@ def tuner():
         "Missing Arguments: ON\OFF")
         os.close(device)
 
+    except KeyError:
+        os.system("clear")
+        print("\033[93mInvalid Argument\033[0m:",
+        f"\033[93m{sys.argv[2].upper()}\033[0m is not a valid argument!",
+        "Just \033[93mON\033[0m or \033[93mOFF\033[0m!")
+        os.close(device)
+
+def patch():
+    def list():
+        for patch in zoom.patch:
+            print(patch)
+            if patch[-1] == "9":
+                print("===")
+
+    try:
+        if sys.argv[2] == 'list':
+            list()
+            return
+
+        os.write(device, bytearray(zoom.patch[sys.argv[2].upper()]))
+        os.close(device)
+
+    except IndexError:
+        os.system("clear")
+        print("\033[93mWarning!\033[0m",
+        "Missing Arguments: Patch","\n")
+        print("patch list for list of patch")
+        os.close(device)
+
+    except KeyError:
+        os.system("clear")
+        print("\033[93mWarning!\033[0m",
+        f"Patch: \033[93m{sys.argv[2].upper()}\033[0m path not found.","\n")
+        print("patch list for list of patch")
+        os.close(device)
+
 cmd = {
-    'tuner': tuner
+    'tuner': tuner,
+    'patch': patch,
+    'edit': edit_mode
 }
 
-try:
-    cmd[sys.argv[1]]()
-
-except IndexError:
-    os.system("clear")
-    print("\033[93m\t\tCOMMANDS\033[0m")
-    print("tuner [ON/OFF]")
+cmd[sys.argv[1]]()
